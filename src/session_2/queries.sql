f2f022e229907631ff50506d62e2fe1671d94223
Rezar Dalipaj <rezar.dalipaj@fti.edu.al>
2022-06-10 23:21:22 +0200

    queries.sql edited

--- src/session_2/queries.sql
+++ src/session_2/queries.sql
@@ -1,38 +1,42 @@
 use classicmodels;
--- kerkesa A)
-select max(nrOfOrders) AS maxOrders, customerName from(select COUNT(orderNumber) AS nrOfOrders, customerName from orders
+-- query 1)
+select max(nrOfOrders) AS maxOrders, customerName from(select COUNT(orderNumber) AS nrOfOrders, customers.customerNumber, customerName from orders
 JOIN customers
 ON orders.customerNumber = customers.customerNumber
-group by customerName) as T
+group by customerNumber
+order by nrOfOrders DESC) as T
 having maxOrders = max(nrOfOrders);
--- kerkesa B)
-select customers.customerName, customers.country, O.customerNumber, O.orderNumber, O.productCode, O.quantityOrdered, O.priceEach, O.orderLineNumber
-from customers
-JOIN (select orders.customerNumber, orderdetails.orderNumber, productCode, quantityOrdered, priceEach, orderLineNumber   from orders
-JOIN orderdetails
-ON orders.orderNumber = orderdetails.orderNumber) AS O
-WHERE country = 'Germany';
--- kerkesa C)
-select employees.employeeNumber, employees.firstName, employees.lastName, N.paymentDate, N.amount from employees
-JOIN (select payments.customerNumber, payments.amount, payments.paymentDate, customers.salesRepEmployeeNumber from payments
-JOIN customers
-ON customers.customerNumber = payments.customerNumber) AS N;
--- kerkesa D)
-select M.orderNumber, products.productName, M.orderDate from products
-JOIN (select orders.orderNumber, orderdetails.productCode, orders.orderDate from orders
-JOIN orderdetails
-ON orders.orderNumber = orderdetails.orderNumber
-where orderDate < '2005-01-01' and orderDate > '2004-11-30') AS M
-group by productName;
--- kerkesa E)
-create table employeedetails AS
-select employeeNumber, email as personalEmail from
-employees;
-alter table employeedetails
-add bankAccount text;
-alter table employeedetails
-add address text;
-alter table employeedetails
-add phoneNumber text;
+-- query 2)
+select c.customerNumber, c.customerName, c.country, odet.*
+from customers as c
+join orders as o
+join orderdetails as odet
+on c.customerNumber = o.customerNumber and o.orderNumber = odet.orderNumber
+where c.country = 'Germany';
+-- query 3)
+select emp.employeeNumber, emp.firstName, emp.lastName, SUM(pay.amount) as totalAmount
+from payments as pay
+join customers as c
+join employees as emp
+on pay.customerNumber = c.customerNumber and c.salesRepEmployeeNumber = emp.employeeNumber
+group by emp.employeeNumber;
+-- query 4)
+select p.productCode, p.productName, o.orderDate
+from products as p
+join orderdetails as odet
+join orders as o
+on p.productCode = odet.productCode and o.orderNumber = odet.orderNumber
+where o.orderDate < '2005-01-01' and orderDate > '2004-11-30';
+-- query 5)
+drop table employeedetails;
+create table employeedetails (
+employeedetailsId int,
+employeeNumber int,
+bankAccount text,
+address text,
+phoneNumber text,
+primary key(employeedetailsId),
+foreign key(employeeNumber) references employees(employeeNumber)
+)
 select * from employeedetails;
 
